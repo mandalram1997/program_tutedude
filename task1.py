e4bf80d58f73e7d5ceae66500bf639a0f6a68012
SIBARAM MANDAL <mandalram.siba@gmail.com>
2025-03-06 13:52:19 +0530

    Add files via upload
    Signed-off-by: SIBARAM MANDAL <mandalram.siba@gmail.com>

---
+++ task1.py
@@ -0,0 +1,24 @@
+# Task 1: Perform Basic Mathematical Operations
+x= int(input("enter First number : "))
+y = int(input("enter secoand number : "))
+print(f"two number's {x,y}")
+
+'''Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+'''
+print(f'addition : {x+y}',f'\nSubstraction : {x-y}',f'\nMultiplication : {x*y}',f'\nDivision : {x/y}')
+
+# Task 2: Create a Personalized Greeting
+
+First_name= input("enter First Name : ")
+Last_name = input("enter last Name : ")
+
+print(f"Hello, {First_name,Last_name}! Welcome to the Python program.")
+
+
+
+
+
---
+++ task2.py
@@ -0,0 +1,6 @@
+# Task 2: Create a Personalized Greeting
+
+First_name= input("enter First Name : ")
+Last_name = input("enter last Name : ")
+
+print(f'Hello, {First_name+" "+Last_name}! Welcome to the Python program.')
