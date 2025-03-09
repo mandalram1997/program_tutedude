2904b5915d719aa6d89df9c39ddaf10ef4daed9a
SIBARAM MANDAL <mandalram.siba@gmail.com>
2025-03-09 13:33:24 +0530

    Update task1.py
    Signed-off-by: SIBARAM MANDAL <mandalram.siba@gmail.com>

--- task1.py
+++ task1.py
@@ -10,12 +10,6 @@ o	Division
 '''
 print(f'Addition : {x+y}',f'\nSubstraction : {x-y}',f'\nMultiplication : {x*y}',f'\nDivision : {x/y}')
 
-# Task 2: Create a Personalized Greeting
-
-First_name = input("enter First Name : ")
-Last_name = input("enter last Name : ")
-
-print(f"Hello, {First_name,Last_name}! Welcome to the Python program.")
 
 
 
